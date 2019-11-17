4384a819cec8b38db389c12497c018d64e55827d
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2019-11-16 22:43:30 -0500

    Added image screen

--- Chapter1/rn-starter/src/screens/HomeScreen.js
+++ Chapter1/rn-starter/src/screens/HomeScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (  
   <View>
     <Text style={styles.text}>PogChad</Text>
     <Button 
       title="Go To Components Demo"
-      onPress={() => props.navigation.navigate('Components')}
+      onPress={() => navigation.navigate('Components')}
     />
-    <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
-      <Text>Go to List Demo</Text>
-    </TouchableOpacity>
+    <Button 
+      title='Go to List Demo'
+      onPress={() => navigation.navigate('List')}
+    />
+    <Button
+      title='Navigate to Image Screen'
+      onPress={() => navigation.navigate('Image')}
+    />
   </View>
   );
 };
