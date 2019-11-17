e4e9e64e93978fc84c8bf24d87bfaf33e01dda9e
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2019-11-16 21:15:44 -0500

    Added flatlist with a couple of extra props

--- Chapter1/rn-starter/src/screens/ListScreen.js
+++ Chapter1/rn-starter/src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1' },
-    { name: 'Friend #2' },
-    { name: 'Friend #3' },
-    { name: 'Friend #4' },
-    { name: 'Friend #5' },
-    { name: 'Friend #6' },
-    { name: 'Friend #7' },
-    { name: 'Friend #8' },
-    { name: 'Friend #9' },
+    { name: 'Friend 1', age: 'Age 20' },
+    { name: 'Friend 2', age: 'Age 10' },
+    { name: 'Friend 3', age: 'Age 22' },
+    { name: 'Friend 4', age: 'Age 14' },
+    { name: 'Friend 5', age: 'Age 32' },
+    { name: 'Friend 6', age: 'Age 16' },
+    { name: 'Friend 7', age: 'Age 45' },
+    { name: 'Friend 8', age: 'Age 72' },
+    { name: 'Friend 9', age: 'Age 11' },
   ];
 
   return (
@@ -19,14 +19,16 @@ const ListScreen = () => {
       keyExtractor={(friend) => friend.name}
       data={friends} 
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return <Text>{item.name} - {item.age}</Text>;
       }}
     />
   );
 }
 
 const styles = StyleSheet.create({
-
+  textStyle: {
+    marginVertical: 50
+  }
 });
 
 export default ListScreen;
