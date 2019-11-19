52be2bb4d98fd74d159d87eae6ee3028662c4c85
TristanR99 <tristanrebello@gmail.com>
2019-11-18 20:41:39 -0500

    Added small counter

---
+++ Chapter1/rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const CounterScreen = () => {
+  // todo: fix this
+  let counter = 0;
+
+  return (
+    <View>
+      <Button
+        title="Increase"
+        onPress={() => {
+          counter++;
+          console.log(counter);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          counter--;
+        }}
+      />
+      <Text>Current count: {counter}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
--- Chapter1/rn-starter/src/screens/HomeScreen.js
+++ Chapter1/rn-starter/src/screens/HomeScreen.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-  return (  
-  <View>
-    <Text style={styles.text}>PogChad</Text>
-    <Button 
-      title="Go To Components Demo"
-      onPress={() => navigation.navigate('Components')}
-    />
-    <Button 
-      title='Go to List Demo'
-      onPress={() => navigation.navigate('List')}
-    />
-    <Button
-      title='Navigate to Image Screen'
-      onPress={() => navigation.navigate('Image')}
-    />
-  </View>
+  return (
+    <View>
+      <Text style={styles.text}>PogChamp</Text>
+      <Button
+        title="Go To Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      <Button
+        title="Navigate to Image Screen"
+        onPress={() => navigation.navigate("Image")}
+      />
+      <Button
+        title="Navigate to Counter Demo"
+        onPress={() => navigation.navigate("Counter")}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
+  },
+  margin: {
+    marginVertical: 3
   }
 });
 
