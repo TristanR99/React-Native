605375d85c209100552316605db31255b10f2c92
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2019-11-22 21:06:04 -0500

    Added useState() Hook

--- Chapter1/rn-starter/src/screens/CounterScreen.js
+++ Chapter1/rn-starter/src/screens/CounterScreen.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
-  // todo: fix this
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          counter++;
-          console.log(counter);
+          setCounter(counter + 1);
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          counter--;
+          setCounter(counter - 1);
         }}
       />
       <Text>Current count: {counter}</Text>
